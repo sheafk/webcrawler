cea65f4251c30dfe099a367ae0cd4a4267d4f4b8
sheafk <sheafureyking@gmail.com>
2017-02-16 18:51:15 -0500

    crawl web first try

--- python-virtual-environments/webcrawler.py
+++ python-virtual-environments/webcrawler.py
@@ -2,3 +2,21 @@ import requests
 
 r = requests.get('http://unhackathon.org/')
 
+def crawl_web(initial_url):
+    crawled, to_crawl = [], []
+    to_crawl.append(initial_url)
+
+    while to_crawl:
+        current_url = to_crawl.pop(0)
+        r = requests.get(current_url)
+        crawled.append(current_url)
+        for url in re.findall('<a href="([^"]+)">', str(r.content)):
+            if url[0] == '/':
+                url = current_url + url
+            pattern = re.compile('https?')
+            if pattern.match(url):
+                to_crawl.append(url)
+    return crawled
+
+print(crawl_web('http://unhackathon.org'))
+
